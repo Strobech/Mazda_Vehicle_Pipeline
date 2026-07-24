417c74f04e8670ddbb27a1bd9aed683a87e42fe9
mikkel <mikkel@wallbreakermedia.com>
2026-07-24 10:57:39 +0200

    0014 Add ManifestField enum

--- src/mazdavp/builder/fields.py
+++ src/mazdavp/builder/fields.py
@@ -1,8 +1,13 @@
-VALID_FIELDS = {
-    "model_code",
-    "spec_code",
-    "model_year",
-    "market",
-    "trim",
-    "colour",
-}
+from enum import StrEnum
+
+
+class ManifestField(StrEnum):
+    MODEL_CODE = "model_code"
+    SPEC_CODE = "spec_code"
+    MODEL_YEAR = "model_year"
+    MARKET = "market"
+    TRIM = "trim"
+    COLOUR = "colour"
+
+
+VALID_FIELDS = {field.value for field in ManifestField}
